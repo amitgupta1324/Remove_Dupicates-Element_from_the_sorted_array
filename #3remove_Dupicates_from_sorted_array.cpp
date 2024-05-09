89d458a6109cbb9a798e6e8ce89a7aaa8ffc8181
Amit Gupta <125515651+amitgupta1324@users.noreply.github.com>
2024-05-09 16:15:56 +0530

    Add files via upload

---
+++ #3remove_Dupicates_from_sorted_array.cpp
@@ -0,0 +1,16 @@
+class solution{
+    public:
+    int removeDuplicates(vectors<int)& nums{
+        if(nums.size()== 0){
+            return 0;
+        }
+int l=1;
+for (int r = 1; r < nums.size(); r++){
+if(nums[r]!=nums[r-1]){
+    nums[l]=nums[r];
+    l++;
+    }
+}
+return l;
+    }
+};
