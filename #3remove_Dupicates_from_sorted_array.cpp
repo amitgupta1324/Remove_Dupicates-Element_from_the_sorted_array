5baec3cb63f95312c6c7e95e276b91941c00b0ff
Amit Gupta <125515651+amitgupta1324@users.noreply.github.com>
2025-02-21 14:04:40 +0530

    Update #3remove_Dupicates_from_sorted_array.cpp

--- #3remove_Dupicates_from_sorted_array.cpp
+++ #3remove_Dupicates_from_sorted_array.cpp
@@ -4,7 +4,7 @@ class solution{
         if(nums.size()== 0){
             return 0;
         }
-int l=1;
+int l=1;  
 for (int r = 1; r < nums.size(); r++){
 if(nums[r]!=nums[r-1]){
     nums[l]=nums[r];
